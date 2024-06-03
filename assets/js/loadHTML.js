f130a0ca29a41dc9ab051205a88b855f20a206e5
Daniel Arcanjo <danielarcanjo1@gmail.com>
2024-06-03 11:06:52 -0300

    feat: add display lead register, and some refactors

---
+++ assets/js/loadHTML.js
@@ -0,0 +1,21 @@
+
+document.addEventListener("DOMContentLoaded", () =>{
+    document.querySelector(".leads").addEventListener("click", () =>{
+        console.log("CLICOU")
+        const path = "../html/register-lead.html"
+        
+        fetch(path)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.querySelector(".page-content-center").innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Erro ao carregar o formulário:', error);
+        });
+    })
+})

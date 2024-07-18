0750f2dd3526ac906ee719af26e11539bb889c5e
danielarcanjo1 <danielarcanjo1@gmail.com>
2024-07-18 20:46:00 -0300

    update

--- frontend/assets/js/logOut.js
+++
@@ -1,8 +0,0 @@
-setTimeout(() =>{
-
-    const logout = document.querySelector(".logout")
-    
-    logout.addEventListener("click", () =>{
-        window.location.href = "../login.html"
-    })
-},1000)
